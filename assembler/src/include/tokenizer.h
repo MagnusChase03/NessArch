1cf8689d14d0fab91a07961587a0de883b60916c
Zachary <zachness2003@gmail.com>
2023-09-18 17:57:42 -0500

    Lexer

---
+++ assembler/src/include/tokenizer.h
@@ -0,0 +1,59 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+typedef enum {
+
+    BIN_IMMIDIATE,
+    HEX_IMMIDIATE,
+    REGISTER,
+    INSTRUCTION,
+    LABEL,
+    LABEL_JUMP
+
+} TOKEN_TYPE;
+
+typedef struct {
+
+    TOKEN_TYPE token_type;
+    unsigned int length;
+
+} TOKEN;
+
+typedef struct {
+
+    TOKEN* tokens;
+    unsigned int length;
+
+} TOKEN_GROUP;
+
+char DIGITS[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char ALPHA[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+char HEX_PREFIX[3] = "0x";
+char BIN_PREFIX[3] = "0b";
+
+char LABEL_END = ':';
+
+int NUM_REGISTERS = 4;
+char REGISTERS[][3] = {"ax", "bx", "cx", "dx"};
+
+int NUM_INSTRUCTIONS = 12;
+char INSTRUCTIONS[][5] = {
+    "mov",
+    "add",
+    "sub",
+    "and",
+    "or",
+    "xor",
+    "push",
+    "pop",
+    "cmp",
+    "j",
+    "jz",
+    "jnz"
+};
+
+TOKEN_GROUP tokenize(const char* str, unsigned int length);
+TOKEN lex(const char* str, unsigned int length);
+
+#endif
---
+++ assembler/src/tokenizer.c
@@ -0,0 +1,179 @@
+#include "./include/tokenizer.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void lex_error(const char* str) {
+
+    printf("[LEX ERROR] %s\n", str);
+    exit(1);
+
+}
+
+int is_digit(char x) {
+    for (int i = 0; i < sizeof(DIGITS); i++) {
+        if (DIGITS[i] == x) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int is_alpha(char x) {
+    for (int i = 0; i < sizeof(ALPHA); i++) {
+        if (ALPHA[i] == x) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int is_hex(char x) {
+    if (is_digit(x)) {
+        return 1;
+    }
+
+    for (int i = 0; i < 6; i++) {
+        if (ALPHA[i] == x) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int is_register(const char* str) {
+    for (int i = 0; i < NUM_REGISTERS; i++) {
+        if (strcmp(str, REGISTERS[i]) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int is_instruction(const char* str) {
+    for (int i = 0; i < NUM_INSTRUCTIONS; i++) {
+        if (strcmp(str, INSTRUCTIONS[i]) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+TOKEN_GROUP tokenize(const char* str, unsigned int length) {
+    TOKEN_GROUP tokens;
+
+    unsigned int start = 0;
+    while (str[start] == ' ') {
+        start += 1;
+    }
+
+    unsigned int num_tokens = 1;
+    for (int i = start; i < length; i++) {
+        if (str[i] == ' ') {
+            num_tokens += 1;
+        }
+    }
+    tokens.length = num_tokens;
+    tokens.tokens = (TOKEN*) malloc(sizeof(TOKEN) * num_tokens);
+
+    char buffer[100];
+    for (int i = 0; i < sizeof(buffer); i++) {
+        buffer[i] = '\0';
+    }
+
+    unsigned int token_index = 0;
+    int buffer_index = 0;
+    for (int i = start; i < length; i++, buffer_index++) {
+        if (str[i] == ' ') {
+            tokens.tokens[token_index] = lex(buffer, strlen(buffer));
+            buffer_index = -1;
+            token_index += 1;
+            for (int i = 0; i < sizeof(buffer); i++) {
+                buffer[i] = '\0';
+            }
+        } else {
+            buffer[buffer_index] = str[i];
+        }
+    }
+    tokens.tokens[token_index] = lex(buffer, strlen(buffer));
+
+    return tokens;
+}
+
+TOKEN lex(const char* str, unsigned int length) {
+    TOKEN token;
+    token.length = length;
+
+    if (is_alpha(str[0])) {
+
+        if (is_register(str)) {
+            token.token_type = REGISTER;
+            return token;
+        } else if (is_instruction(str)) {
+            token.token_type = INSTRUCTION;
+            return token;
+        } else {
+
+            for (int i = 1; i < length; i++) {
+                if (!is_alpha(str[i])) {
+                    if (str[i] == LABEL_END && i == length - 1) {
+                        token.token_type = LABEL;
+                        return token;
+                    } else {
+                        lex_error(str);
+                    }
+                }
+            }
+            
+            token.token_type = LABEL_JUMP;
+            return token;
+        }
+
+    } else if (str[0] == '0' && length >= 3) {
+
+        if (str[1] == 'b') {
+
+            for (int i = 2; i < length; i++) {
+                if (str[i] != '0' && str[i] != '1') {
+                    lex_error(str);
+                }
+            }
+
+            token.token_type = BIN_IMMIDIATE;
+            return token;
+
+        } else if (str[1] == 'x') {
+
+            for (int i = 2; i < length; i++) {
+                if (!is_hex(str[i])) {
+                    lex_error(str);
+                }
+            }
+
+            token.token_type = HEX_IMMIDIATE;
+            return token;
+
+        }
+
+    }
+
+    lex_error(str);
+
+}
+
+int main() {
+
+    char instruction[] = "test: jz test";
+    TOKEN_GROUP tokens = tokenize(instruction, strlen(instruction));
+
+    for (int i = 0; i < tokens.length; i++) {
+        printf("[TOKEN] %d\n", tokens.tokens[i].token_type);
+    }
+
+}
